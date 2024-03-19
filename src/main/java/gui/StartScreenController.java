d119695e8cfc4b7fa1574ef6e242d1f49840df01
rhit-edgingza <edgingza@rose-hulman.edu>
2024-03-19 15:06:57 -0400

    Got rid of top ribbon

--- src/main/java/gui/StartScreenController.java
+++ src/main/java/gui/StartScreenController.java
@@ -9,6 +9,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.util.ResourceBundle;
